8ca98f05825083e4025fe3b626d7dd2866c4205a
Mateo Álvarez <malvarez@prexcard.com>
2023-05-11 20:48:58 -0300

    TransactionPolicy

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -6,9 +6,11 @@ namespace App\Providers;
 
 use App\Models\Buyer;
 use App\Models\Seller;
+use App\Models\Transaction;
 use App\Models\User;
 use App\Policies\BuyerPolicy;
 use App\Policies\SellerPolicy;
+use App\Policies\TransactionPolicy;
 use App\Policies\UserPolicy;
 use Carbon\Carbon;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
@@ -24,7 +26,8 @@ class AuthServiceProvider extends ServiceProvider
     protected $policies = [
         Buyer::class => BuyerPolicy::class,
         Seller::class => SellerPolicy::class,
-        User::class => UserPolicy::class
+        User::class => UserPolicy::class,
+        Transaction::class => TransactionPolicy::class,
     ];
 
     /**
