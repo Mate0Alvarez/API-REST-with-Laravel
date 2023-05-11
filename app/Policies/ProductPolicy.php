679b3f8c84216e5559088d0ee68d4c1ab7d8b866
Mateo Álvarez <malvarez@prexcard.com>
2023-05-11 20:55:52 -0300

    ProductPolicy

---
+++ app/Policies/ProductPolicy.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\Product;
+use App\Models\User;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class ProductPolicy
+{
+    use HandlesAuthorization;
+
+    /**
+     * Determine whether the user can add a category to a product
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\Product  $product
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function addCategory(User $user, Product $product)
+    {
+        return $user->id === $product->seller->id;
+    }
+
+    /**
+     * Determine whether the user can remove a category from a product.
+     *
+     * @param  \App\Models\User  $user
+     * @param \App\Models\Product $product
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function deleteCategory(User $user, Product $product)
+    {
+        return $user->id === $product->seller->id;
+    }
+}
--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -5,10 +5,12 @@ namespace App\Providers;
 // use Illuminate\Support\Facades\Gate;
 
 use App\Models\Buyer;
+use App\Models\Product;
 use App\Models\Seller;
 use App\Models\Transaction;
 use App\Models\User;
 use App\Policies\BuyerPolicy;
+use App\Policies\ProductPolicy;
 use App\Policies\SellerPolicy;
 use App\Policies\TransactionPolicy;
 use App\Policies\UserPolicy;
@@ -28,6 +30,7 @@ class AuthServiceProvider extends ServiceProvider
         Seller::class => SellerPolicy::class,
         User::class => UserPolicy::class,
         Transaction::class => TransactionPolicy::class,
+        Product::class => ProductPolicy::class,
     ];
 
     /**
